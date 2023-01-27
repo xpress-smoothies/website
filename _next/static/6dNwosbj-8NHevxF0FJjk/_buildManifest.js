513a4a3b371b1e36ee19e625cf018a2d80301121
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-01-27 13:42:28 -0500

    Updates

--- _next/static/6dNwosbj-8NHevxF0FJjk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/361-0e454ddaa2778e0d.js","static/chunks/pages/index-2bb006412390af3b.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
