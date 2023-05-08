10646e7c41f5ff4ca097300ee8aec7465af63781
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-05-08 13:30:11 -0400

    hot hero carousel

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 
+import { Carousel } from 'react-responsive-carousel';
+
 import config from '../config/index.json';
 
 const MainHeroImage = () => {
   const { mainHero } = config;
+
+  function carousel() {
+    return (
+      <Carousel
+        emulateTouch={true}
+        autoPlay={true}
+        interval={4000}
+        infiniteLoop={true}
+      >
+        {mainHero.heroImages.map((heroImg, index) => {
+          return (
+            <div key={index} className="mx-auto overflow-hidden">
+              <div className="md:hidden">
+                <img
+                  className="h-1/2 w-full object-cover lg:w-full lg:h-full"
+                  src={heroImg.imgPathS}
+                />
+              </div>
+
+              <div className="hidden md:inline-flex">
+                <img src={heroImg.imgPathM} />
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
+    );
+  }
+
+  function carouselBig() {
+    return (
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 hidden md:block overflow-hidden">
+        {carousel()}
+      </div>
+    );
+  }
+
+  function carouselSmall() {
+    return (
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 md:hidden overflow-hidden">
+        <div className="h-1/2 w-full object-cover md:h-96 lg:w-full lg:h-full overflow-hidden">
+          {carousel()}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 sm:hidden">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src={mainHero.imgS}
-          alt="happy team image"
-        />
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 hidden sm:block">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src={mainHero.img}
-          alt="happy team image"
-        />
-      </div>
+      {carouselBig()}
+      {carouselSmall()}
     </>
   );
 };
